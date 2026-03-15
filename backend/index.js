b5f74a4668e2bb20e3ce97b240f042917eefe580
benshabbat <benshabbat27@gmail.com>
2026-03-15 10:20:40 +0200

    feat: set up Express server with dotenv configuration and welcome route

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/",(req,res,next)=>{
+    res.status(200).json({message:"Welcome"})
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
+
+
