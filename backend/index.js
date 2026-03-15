ee3d6ae0f24d2119b810583f97000b97fa1c495f
benshabbat <benshabbat27@gmail.com>
2026-03-15 10:32:35 +0200

    feat: implement MongoDB connection logic in a new config file

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,18 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
-dotenv.config()
+dotenv.config();
 
+const app = express();
 
-const app = express()
+app.use(express.json());
 
-app.use(express.json())
+app.get("/", (req, res, next) => {
+  res.status(200).json({ message: "Welcome" });
+});
 
-app.get("/",(req,res,next)=>{
-    res.status(200).json({message:"Welcome"})
-})
-
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on port ${process.env.PORT}`)
-})
-
-
+app.listen(process.env.PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
